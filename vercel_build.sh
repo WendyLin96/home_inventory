9afbfadd0f90dcaf72b787b4b51591953da10081
Wendy Lin <wenhsuan1201@gmail.com>
2025-12-13 14:49:23 +1000

    Set Supabase URL&Key

--- vercel_build.sh
+++ vercel_build.sh
@@ -12,4 +12,4 @@ export PATH="$PATH:`pwd`/flutter/bin"
 
 flutter --version
 flutter pub get
-flutter build web
+flutter build web --dart-define=SUPABASE_URL=https://tyzoyeplrkfgokezlaqy.supabase.co --dart-define=SUPABASE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5em95ZXBscmtmZ29rZXpsYXF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzNDg1MDAsImV4cCI6MjA4MDkyNDUwMH0.0uJ1DxKV4jW1R-lZLVWEDRwXjWqwTwhkGx3DSRfiC4c
