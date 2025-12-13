0e2b3888156da44b89fd65928751261b7ba32fc6
Wendy Lin <wenhsuan1201@gmail.com>
2025-12-13 14:16:44 +1000

    clone + cache

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Downloading Flutter..."
-git clone https://github.com/flutter/flutter.git -b stable --depth 1
+if [ ! -d flutter ]; then
+  echo "Downloading Flutter..."
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1
+else
+  echo "Flutter cached, skip download"
+fi
 
 export PATH="$PATH:`pwd`/flutter/bin"
 
